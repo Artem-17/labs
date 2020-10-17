c6277d3ff64c275af33caf9a430b0be91db1b97a
Artem <matarasartem@yandex.by>
2020-10-17 17:21:37 +0300

    lab1

---
+++ lab1/TrapezeApp/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrapezeLib;
+
+namespace TrapezeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trapeze trapeze = CreateTrapeze();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1.Calculate the side lengths of a trapezoid");
+                Console.WriteLine("2.Calculate the area of ​​a trapezoid");
+                Console.WriteLine("3.Calculate the perimeter of a trapezoid");
+                Console.WriteLine("4.Check if point belongs to trapezoid");
+                Console.WriteLine("5.Create a new trapezoid");
+                Console.WriteLine("Press the Enter key to exit");
+                int n;
+                try
+                {
+                    n = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    break;
+                }
+                switch (n)
+                {
+                    case 1:
+                        Console.WriteLine("Side lengths of trapezoid");
+                        int i = 1;
+                        foreach (double side in trapeze.FindSidesLen())
+                        {
+                            Console.WriteLine("Party number" + Convert.ToString(i) + " = " + Convert.ToString(side));
+                            i += 1;
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("The area of ​​the trapezoid is " + trapeze.FindSquare());
+                        break;
+                    case 3:
+                        Console.WriteLine("The perimeter of the trapezoid is " + trapeze.FindPerimeter());
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter point x and y coordinates");
+                        double x, y;
+                        while (true)
+                        {
+                            try
+                            {
+                                Console.WriteLine("Enter the x coordinate");
+                                x = Convert.ToDouble(Console.ReadLine());
+                                Console.WriteLine("Enter y coordinate");
+                                y = Convert.ToDouble(Console.ReadLine());
+                                break;
+                            }
+                            catch
+                            {
+                                Console.WriteLine("You entered incorrect values");
+                            }
+                        }
+                        if (trapeze.IsPoint(x, y))
+                            Console.WriteLine("Dot " + Convert.ToString(x) + "; " + Convert.ToString(y) + " belongs to the trapezoid");
+                        else
+                            Console.WriteLine("Dot " + Convert.ToString(x) + "; " + Convert.ToString(y) + " does not belong to a trapezoid");
+                        break;
+                    case 5:
+                        trapeze = CreateTrapeze();
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+        static Trapeze CreateTrapeze()
+        {
+            Console.WriteLine("Create a curved trapezoid bounded by the OX axis and sin function");
+            Console.WriteLine("Enter the boundaries of the trapezoid (x1, x2)");
+            Trapeze trapeze;
+            double x1, x2;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the left border of the trapezoid x1");
+                    x1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter the right border of the trapezoid x2");
+                    x2 = Convert.ToDouble(Console.ReadLine());
+                    trapeze = new Trapeze(x1, x2);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("You entered incorrect values");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("This trapezoid does not exist");
+                }
+            }
+            return trapeze;
+        }
+    }
+}
---
+++ lab1/TrapezeLib/Trapeze.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrapezeLib
+{
+    /// <summary>
+    /// Класс, представляющий собой трапецию, образованную осью OX и функцией синус
+    /// </summary>
+    public class Trapeze
+    {
+        /// <summary>
+        /// x1 и x2 - границы криволинейной трапеции
+        /// </summary>
+        private readonly double x1, x2;
+        /// <summary>
+        /// конструктор класса Трапеция
+        /// Если границы переданы в другом порядке, происходит сортировка
+        /// </summary>
+        /// <param name="x1">Левая граница трапеции</param>
+        /// <param name="x2">Правая граница трапеции</param>
+        public Trapeze(double x1, double x2)
+        {
+            this.x1 = Math.Min(x1, x2);
+            this.x2 = Math.Max(x1, x2);
+            if (!this.IsExist())
+            {
+                throw new Exception("This trapeze doesn't exist");
+            }
+        }
+        /// <summary>
+        /// Метод, проверяющий сущетвует ли трапеция
+        /// </summary>
+        public bool IsExist()
+        {
+            if (((Math.Sin(x1) < 0 && Math.Sin(x2) < 0) || (Math.Sin(x1) > 0 && Math.Sin(x2) > 0)) && (x2 - x1) < Math.PI)
+                return true;
+            else
+                return false;
+        }
+        /// <summary>
+        /// Метод, который находит длину стороны трапеции, образованной функцией синус
+        /// </summary>
+        /// <param name="startX">Левая граница кривой</param>
+        /// <param name="finishX">Правая граница кривой</param>
+        /// <returns>Длина интегральной кривой</returns>
+        private double FindIntegLen()
+        {
+            double len = 0;
+            double dx = (x2 - x1) / 1000;
+            double startX = x1;
+            double finishX = x1 + dx;
+            while (finishX < x2)
+            {
+                len += Math.Sqrt(Math.Pow(finishX - startX, 2) + Math.Pow(Math.Sin(finishX) - Math.Sin(startX), 2));
+                startX += dx;
+                finishX += dx;
+            }
+            return len;
+        }
+        /// <summary>
+        /// Метод, вычисляющий длины сторон трапеции
+        /// </summary>
+        /// <returns>Длины сторон трапеции</returns>
+        public double[] FindSidesLen()
+        {
+            double[] sides = new double[4];
+            sides[0] = x2 - x1;
+            sides[1] = Math.Abs(Math.Sin(x2));
+            sides[2] = FindIntegLen();
+            sides[3] = Math.Abs(Math.Sin(x1));
+            return sides;
+        }
+        /// <summary>
+        /// Метод, считающий площадь трапеции по формуле определенного интеграла
+        /// </summary>
+        /// <returns>Площадь трапеции</returns>
+        public double FindSquare()
+        {
+            return Math.Cos(x1) - Math.Cos(x2);
+        }
+        /// <summary>
+        /// Метод, вычисляющий периметр трапеции
+        /// </summary>
+        /// <returns>Периметр трапеции</returns>
+        public double FindPerimeter()
+        {
+            double perimeter = 0;
+            foreach (double side in this.FindSidesLen())
+                perimeter += side;
+            return perimeter;
+        }
+        /// <summary>
+        /// Метод, проверяющий, принадлежит ли точка трапеции
+        /// </summary>
+        /// <param name="x">Координата точки по оси X</param>
+        /// <param name="y">Координата точки по оси Y</param>
+        public bool IsPoint(double x, double y)
+        {
+            if (Math.Sin(x1) > 0)
+            {
+                if (y < 0 || y > Math.Sin(x) || x < x1 || x > x2)
+                    return false;
+                else
+                    return true;
+            }
+            else
+            {
+                if (y > 0 || y < Math.Sin(x) || x < x1 || x > x2)
+                    return false;
+                else
+                    return true;
+            }
+        }
+        /// <summary>
+        /// Переопределил метод, что бы красиво выводит трапецию
+        /// </summary>
+        /// <returns>Строковое представление класса трапеция</returns>
+        public override string ToString()
+        {
+            return "It is a trapeze on the interval [" + Convert.ToString(x1) + "; " + Convert.ToString(x2) + "]";
+        }
+    }
+}
